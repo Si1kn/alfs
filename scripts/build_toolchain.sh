786671b2aea5ba7f8520cb506b442ed5ead1f29a
Si1kn <si1kn521@gmail.com>
2022-05-21 13:52:24 +1000

    a

--- scripts/build_toolchain.sh
+++ scripts/build_toolchain.sh
@@ -1,12 +1,12 @@
-$LFS/alfs/toolchain/binutils-pass-1.sh
+source $LFS/alfs/toolchain/binutils-pass-1.sh
 
-$LFS/alfs/toolchain/gcc-pass-1.sh
+source $LFS/alfs/toolchain/gcc-pass-1.sh
 
-$LFS/alfs/toolchain/linux-api-headers.sh
+source $LFS/alfs/toolchain/linux-api-headers.sh
 
-$LFS/alfs/toolchain/glbic.sh
+source $LFS/alfs/toolchain/glbic.sh
 
-$LFS/alfs/toolchain/libstd-pass-1.sh
+source $LFS/alfs/toolchain/libstd-pass-1.sh
 
 echo
 echo "Completed"
